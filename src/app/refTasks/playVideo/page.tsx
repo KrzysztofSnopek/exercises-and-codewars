ef2190799a7494fd2af38ca51c40ab87ac94886a
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-08-17 15:17:54 +0200

    Adds useRef and conditional to the handleClick to check if the video should be played or paused with the next click

--- src/app/refTasks/playVideo/page.tsx
+++ src/app/refTasks/playVideo/page.tsx
@@ -1,26 +1,34 @@
 "use client";
 
-import { useState } from "react";
+import ReturnButton from "@/app/components/ReturnButton";
+import { useRef, useState } from "react";
 
 // In this example, the button toggles a state variable to switch between a playing and a paused state. However, in order to actually play or pause the video, toggling state is not enough. You also need to call play() and pause() on the DOM element for the <video>. Add a ref to it, and make the button work.
 
 export default function VideoPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   function handleClick() {
     const nextIsPlaying = !isPlaying;
     setIsPlaying(nextIsPlaying);
+    if (isPlaying === false && videoRef.current) {
+      videoRef.current.play();
+    } else if (isPlaying === true && videoRef.current) {
+      videoRef.current.pause();
+    }
   }
 
   return (
     <>
       <button onClick={handleClick}>{isPlaying ? "Pause" : "Play"}</button>
-      <video width="250">
+      <video width="250" ref={videoRef}>
         <source
           src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
           type="video/mp4"
         />
       </video>
+      <ReturnButton />
     </>
   );
 }
