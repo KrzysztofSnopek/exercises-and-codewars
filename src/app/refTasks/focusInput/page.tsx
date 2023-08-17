35eb00669cddd8be61ea4141ccc3a7af1d447c24
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-08-17 16:16:37 +0200

    Adds useRef to the input and handleClick that uses it

--- src/app/refTasks/focusInput/page.tsx
+++ src/app/refTasks/focusInput/page.tsx
@@ -2,13 +2,28 @@
 
 // Make it so that clicking the “Search” button puts focus into the field.
 
+import { useRef } from "react";
+
 export default function Page() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleClick() {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }
+
   return (
     <>
       <nav>
-        <button>Search</button>
+        <button
+          onClick={handleClick}
+          className="p-2 mb-2 border border-slate-300 text-slate-300 px-2 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        >
+          Search
+        </button>
       </nav>
-      <input placeholder="Looking for something?" />
+      <input placeholder="Looking for something?" ref={inputRef} />
     </>
   );
 }
