2bd89ea9918624ebd0e535fe14bac1ce1600f733
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-09-05 18:30:25 +0200

    Included useEffectEvent onMount inside uesEffect
    Included useEffectEvent onMount inside useEffect as the code inside needed to be reactive with respect to the delay variable

--- src/app/effectTasks/fixNonAdjustableDelay/page.tsx
+++ src/app/effectTasks/fixNonAdjustableDelay/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+// In this example, you can customize the interval delay. It’s stored in a delay state variable which is updated by two buttons. However, even if you press the “plus 100 ms” button until the delay is 1000 milliseconds (that is, a second), you’ll notice that the timer still increments very fast (every 100 ms). It’s as if your changes to the delay are ignored. Find and fix the bug.
+
 import ReturnButton from "../../components/ReturnButton";
 import { useState, useEffect } from "react";
 import { experimental_useEffectEvent as useEffectEvent } from "react";
@@ -13,29 +15,31 @@ export default function Timer() {
     setCount((c) => c + increment);
   });
 
-  const onMount = useEffectEvent(() => {
-    return setInterval(() => {
+  useEffect(() => {
+    const id = setInterval(() => {
       onTick();
     }, delay);
-  });
-
-  useEffect(() => {
-    const id = onMount();
     return () => {
       clearInterval(id);
     };
-  }, []);
+  }, [delay]);
 
   return (
     <>
       <h1>
         Counter: {count}
-        <button onClick={() => setCount(0)}>Reset</button>
+        <button
+          className="m-2 p-1 border border-slate-300 text-slate-300 px-2 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          onClick={() => setCount(0)}
+        >
+          Reset
+        </button>
       </h1>
       <hr />
       <p>
         Increment by:
         <button
+          className="m-2 p-1 border border-slate-300 text-slate-300 px-2 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
           disabled={increment === 0}
           onClick={() => {
             setIncrement((i) => i - 1);
@@ -45,6 +49,7 @@ export default function Timer() {
         </button>
         <b>{increment}</b>
         <button
+          className="m-2 p-1 border border-slate-300 text-slate-300 px-2 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
           onClick={() => {
             setIncrement((i) => i + 1);
           }}
@@ -55,6 +60,7 @@ export default function Timer() {
       <p>
         Increment delay:
         <button
+          className="m-2 p-1 border border-slate-300 text-slate-300 px-2 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
           disabled={delay === 100}
           onClick={() => {
             setDelay((d) => d - 100);
@@ -64,6 +70,7 @@ export default function Timer() {
         </button>
         <b>{delay} ms</b>
         <button
+          className="m-2 p-1 border border-slate-300 text-slate-300 px-2 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
           onClick={() => {
             setDelay((d) => d + 100);
           }}
