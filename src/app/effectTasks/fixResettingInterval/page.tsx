eff9b428533bcb170fe6dc390d5c31c45f32fc27
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-09-06 19:45:44 +0200

    Gets rid of deps that was forcing a re-render because of a loop and updates the count number inside effect instead

--- src/app/effectTasks/fixResettingInterval/page.tsx
+++ src/app/effectTasks/fixResettingInterval/page.tsx
@@ -10,19 +10,21 @@ export default function Timer() {
 
   useEffect(() => {
     console.log("✅ Creating an interval");
+
     const id = setInterval(() => {
       console.log("⏰ Interval tick");
-      setCount(count + 1);
+      setCount((count) => count + 1);
     }, 1000);
     return () => {
       console.log("❌ Clearing an interval");
       clearInterval(id);
     };
-  }, [count]);
+  }, []);
 
   return (
     <>
-      <h1>Counter: {count}</h1> <ReturnButton />
+      <h1>Counter: {count}</h1>
+      <ReturnButton />
     </>
   );
 }
