0eee992ced495cea4cac70e1ea7ee4051449300a
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-09-14 15:33:19 +0200

    Adds starting code to a challenge to fix a resetting interval

--- src/app/hookTasks/extractUseInterval/page.tsx
+++ src/app/hookTasks/extractUseInterval/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ReturnButton from "@/app/components/ReturnButton.tsx";
 // Currently, your useCounter Hook does two things. It sets up an interval, and it also increments a state variable on every interval tick. Split out the logic that sets up the interval into a separate Hook called useInterval. It should take two arguments: the onTick callback, and the delay. After this change, your useCounter implementation should look like this:
 
 // export function useCounter(delay) {
@@ -14,5 +15,10 @@ import { useCounter } from "./useCounter.ts";
 
 export default function Counter() {
   const count = useCounter(1000);
-  return <h1>Seconds passed: {count}</h1>;
+  return (
+    <div>
+      <h1>Seconds passed: {count}</h1>
+      <ReturnButton />
+    </div>
+  );
 }
---
+++ src/app/hookTasks/fixResettingInterval/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import ReturnButton from "@/app/components/ReturnButton.tsx";
+import { useCounter } from "./useCounter.ts";
+import { useInterval } from "./useInterval.ts";
+
+// In this example, there are two separate intervals.
+
+// The App component calls useCounter, which calls useInterval to update the counter every second. But the App component also calls useInterval to randomly update the page background color every two seconds.
+
+// For some reason, the callback that updates the page background never runs. Add some logs inside useInterval:
+
+//   useEffect(() => {
+//     console.log('✅ Setting up an interval with delay ', delay)
+//     const id = setInterval(onTick, delay);
+//     return () => {
+//       console.log('❌ Clearing an interval with delay ', delay)
+//       clearInterval(id);
+//     };
+//   }, [onTick, delay]);
+// Do the logs match what you expect to happen? If some of your Effects seem to re-synchronize unnecessarily, can you guess which dependency is causing that to happen? Is there some way to remove that dependency from your Effect?
+
+// After you fix the issue, you should expect the page background to update every two seconds.
+
+export default function Counter() {
+  const count = useCounter(1000);
+
+  useInterval(() => {
+    const randomColor = `hsla(${Math.random() * 360}, 100%, 50%, 0.2)`;
+    document.body.style.backgroundColor = randomColor;
+  }, 2000);
+
+  return (
+    <div>
+      <h1>Seconds passed: {count}</h1>
+      <ReturnButton />
+    </div>
+  );
+}
